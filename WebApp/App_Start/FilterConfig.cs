d602ea48433374bde721365b4e4e9ed6a4d59428
Atif Aziz <code@raboof.com>
2017-12-01 14:02:05 +0100

    Restore error logging with custom error pages

--- WebApp/App_Start/FilterConfig.cs
+++ WebApp/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Mvc;
+using Elmah;
 
 namespace WebApp
 {
@@ -7,7 +8,20 @@ namespace WebApp
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new ErrorLogger());
             filters.Add(new HandleErrorAttribute());
+        }
+
+        sealed class ErrorLogger : IExceptionFilter
+        {
+            public void OnException(ExceptionContext context)
+            {
+                if (!context.ExceptionHandled)
+                    return;
+                var httpContext = context.HttpContext.ApplicationInstance.Context;
+                ErrorSignal.FromContext(httpContext)
+                    .Raise(context.Exception, httpContext);
+            }
         }
     }
 }
